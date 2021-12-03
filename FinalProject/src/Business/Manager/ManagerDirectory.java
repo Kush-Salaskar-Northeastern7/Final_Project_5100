05c1fda198d174aff51eea78f7ce9bdc9a728466
Kush Salaskar <salaskar.k@northeastern.edu>
2021-12-03 01:09:27 -0500

    Manager model

--- FinalProject/src/Business/Manager/ManagerDirectory.java
+++ FinalProject/src/Business/Manager/ManagerDirectory.java
@@ -5,10 +5,37 @@
  */
 package Business.Manager;
 
+import Business.UserAccount.UserAccount;
+import java.util.ArrayList;
+
 /**
  *
  * @author kushsalaskar
  */
 public class ManagerDirectory {
+    private ArrayList<Manager> managerList;
     
+    public ManagerDirectory(){
+        managerList = new ArrayList<>();
+    }
+
+    public ArrayList<Manager> getManagerList() {
+        return managerList;
+    }
+
+    public void setManagerList(ArrayList<Manager> managerList) {
+        this.managerList = managerList;
+    }
+    
+    public Manager addManager(String name, UserAccount ua){
+        Manager m = new Manager();
+        m.setManagerName(name);
+        m.setUserAccount(ua);
+        managerList.add(m);
+        return m;
+    }
+    
+    public void deleteManager(Manager m){
+        managerList.remove(m);
+    }
 }
