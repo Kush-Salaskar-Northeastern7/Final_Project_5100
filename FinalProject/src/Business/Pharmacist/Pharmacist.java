b77f97ae085fea9f2377cc53a58682bc147e7525
Anushka Desai <desai.anu@northeastern.edu>
2021-12-11 13:27:25 -0500

    New role : Pharmacist added

---
+++ FinalProject/src/Business/Pharmacist/Pharmacist.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Pharmacist;
+
+import Business.UserAccount.UserAccount;
+
+/**
+ *
+ * @author anushka
+ */
+public class Pharmacist {
+    private String pharmacistName;
+    private UserAccount userAccount;
+    private int DoctorId;
+
+    public String getPharmacistName() {
+        return pharmacistName;
+    }
+
+    public void setPharmacistName(String pharmacistName) {
+        this.pharmacistName = pharmacistName;
+    }
+
+    public UserAccount getUserAccount() {
+        return userAccount;
+    }
+
+    public void setUserAccount(UserAccount userAccount) {
+        this.userAccount = userAccount;
+    }
+
+    public int getDoctorId() {
+        return DoctorId;
+    }
+
+    public void setDoctorId(int DoctorId) {
+        this.DoctorId = DoctorId;
+    }
+    
+    @Override
+    public String toString() {
+        return pharmacistName;
+    }
+}
---
+++ FinalProject/src/Business/Pharmacist/PharmacistDirectory.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Pharmacist;
+
+import Business.UserAccount.UserAccount;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author anushka
+ */
+public class PharmacistDirectory {
+    
+    private ArrayList<Pharmacist> pharmacistList;
+    
+    public PharmacistDirectory(){
+        pharmacistList = new ArrayList();
+    }
+
+    public ArrayList<Pharmacist> getPharmacistList() {
+        return pharmacistList;
+    }
+
+    public void setPharmacistList(ArrayList<Pharmacist> pharmacistList) {
+        this.pharmacistList = pharmacistList;
+    }
+    
+    public Pharmacist createPharmacist(String name, UserAccount ua,int docid){
+        Pharmacist pharmacist = new Pharmacist();
+        pharmacist.setPharmacistName(name);
+        pharmacist.setUserAccount(ua);
+        pharmacist.setDoctorId(docid);
+        pharmacistList.add(pharmacist);
+        return pharmacist;
+    }
+    
+    public void deleteNurse(Pharmacist p){
+        pharmacistList.remove(p);
+    }
+    
+}
